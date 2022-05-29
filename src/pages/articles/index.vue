8e2c5b7c3aab0bae0a30ea2425bf3c7312b6515c
nawawa <oonawa12@gmail.com>
2022-05-30 07:28:45 +0900

    記事一覧取得

--- src/pages/articles/index.vue
+++ src/pages/articles/index.vue
@@ -1,9 +1,27 @@
 <template>
-  
+  <div>
+    <v-col :cols="xs" v-for="article in articles" :key="article.id">
+      <NuxtLink :to="`/articles/${article.id}`">
+        <p>{{ article.title }}</p>
+      </NuxtLink>
+    </v-col>
+  </div>
 </template>
 
 <script>
 export default {
-  
+  async asyncData({ $microcms }) {
+    const data = await $microcms.get({
+      endpoint: `blogs`,
+    });
+    console.log(data);
+    return { articles: data.contents };
+  },
+  computed: {
+    xs() {
+      const breakpoint = this.$vuetify.breakpoint.name;
+      return (breakpoint === `xs`) ? 12: 6;
+    }
+  }
 }
 </script>
