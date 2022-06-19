507836d51a763c657baf1a44225fe7288a6ae93d
nawawa <oonawa12@gmail.com>
2022-06-19 16:02:51 +0900

    取得した記事のスタイリング処理を追加

--- src/components/UI/Article/Page/Content.vue
+++ src/components/UI/Article/Page/Content.vue
@@ -6,6 +6,45 @@
 export default {
   props: {
     content: String
+  },
+  computed: {
+    styling() {
+      return (elements, styles) => {
+          Array.prototype.forEach.call(elements, function(el) {
+          el.style = styles;
+        });
+      };
+    },
+  },
+  mounted() {
+    this.styling(
+      document.getElementsByTagName('h2'),
+      `
+        font-size: 1.6em;
+        line-height: 1.5;
+        margin: 2em 0 1.5rem;
+        font-weight: bold;
+      `
+    );
+
+    this.styling(
+      document.getElementsByTagName('h3'),
+      `
+        font-size: 1.4em;
+        line-height: 1.5;
+        margin: 2em 0 1.5rem;
+        font-weight: bold;
+      `
+    );
+
+    this.styling(
+      document.getElementsByTagName('img'),
+      `
+        margin: 1.5em 0;
+        max-width: 100%;
+        text-align: center;
+      `
+    );
   }
 }
 </script>
