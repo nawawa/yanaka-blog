22832bca03e4c5b2c1b22a672945b82e06bbc2a0
nawawa <oonawa12@gmail.com>
2022-06-23 07:51:42 +0900

    レスポンシブなp要素をラップしたコンポーネントを追加

---
+++ src/components/Pelement.vue
@@ -0,0 +1,10 @@
+<template>
+  <p :style="`font-size:` + ((this.$vuetify.breakpoint.name === `xs`) ? '14': '16') + `px;`">
+    <slot />
+  </p>
+</template>
+
+<script>
+export default {
+}
+</script>
