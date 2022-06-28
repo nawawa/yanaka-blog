07681a292d2a7471fbeacc2580cb3cbedabecbe6
nawawa <oonawa12@gmail.com>
2022-06-29 08:20:40 +0900

    記事一覧画面試し組み

--- src/pages/articles/index.vue
+++ src/pages/articles/index.vue
@@ -1,16 +1,26 @@
 <template>
-  <div>
+  <UIResponsive>
+
+    <h1>新着記事一覧</h1>
+
     <v-row>
       <v-col :cols="xs" v-for="article in articles" :key="article.id">
         <NuxtLink :to="`/articles/${article.id}`">
-          <div>
-            <img :src="article.eyecatch.url" alt="">
-            <p>{{ article.title }}</p>
-          </div>
+          <v-card tile elevation="0" max-width="374" class="my-2">
+            <v-img
+              :src="article.eyecatch.url" alt=""
+            ></v-img>
+            <p>{{ article.category.name }}</p>
+            <v-card-title>{{ article.title }}</v-card-title>
+            <UIArticlePageAuthorList v-for="author in article.author" :key="author.index">
+              <UIArticlePageAuthor :author="author" />
+            </UIArticlePageAuthorList>
+          </v-card>
         </NuxtLink>
       </v-col>
     </v-row>
-  </div>
+
+  </UIResponsive>
 </template>
 
 <script>
@@ -24,7 +34,7 @@ export default {
   computed: {
     xs() {
       const breakpoint = this.$vuetify.breakpoint.name;
-      return (breakpoint === `xs`) ? 12: 6;
+      return (breakpoint === `xs`) ? 12: 3;
     }
   }
 }
