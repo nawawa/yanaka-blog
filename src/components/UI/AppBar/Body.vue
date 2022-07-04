b7019f7f23c462bff678c42649f3901b3eeabc4b
nawawa <oonawa12@gmail.com>
2022-07-04 20:22:24 +0900

    画像を配置するならこんなふうに

--- src/components/UI/AppBar/Body.vue
+++ src/components/UI/AppBar/Body.vue
@@ -1,11 +1,13 @@
 <template>
+    <!-- :style="{background: $vuetify.theme.themes['light'].background}"  -->
   <v-app-bar
-    :style="{background: $vuetify.theme.themes['light'].background}" 
-    :clipped-left="clipped"
-    fixed 
+    absolute 
     flat 
     hide-on-scroll 
-    app
+    app 
+    extended
+    extension-height="394px"
+    :src="require('@/assets/images/test_top_backgroundimage.jpeg')"
   >
     <slot />
   </v-app-bar>
