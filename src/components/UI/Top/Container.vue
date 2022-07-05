4053ca1aa85504ab0957b0828279e54271dd106b
nawawa <oonawa12@gmail.com>
2022-07-06 07:52:28 +0900

    トップページ上下の余白コンポーネントを追加

---
+++ src/components/UI/Top/Container.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+  div {
+    padding: 50px 0 60px;
+  }
+</style>
