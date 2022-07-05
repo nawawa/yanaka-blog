9f5bf13910fe4902f4e591cc65ef9d972e830cdb
nawawa <oonawa12@gmail.com>
2022-07-06 07:52:04 +0900

    文字を黒くしたい場合用のNuxtLinkコンポーネントを追加

---
+++ src/components/UI/StyledNuxtLink.vue
@@ -0,0 +1,19 @@
+<template>
+  <NuxtLink :to="link">
+    <slot />
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+  props: {
+    link: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  a {
+    color: black;
+  }
+</style>
