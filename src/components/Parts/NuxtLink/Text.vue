c0ef90fcf1f1096655d0f91e3c8ae7cace5a7773
nawawa <oonawa12@gmail.com>
2022-07-14 08:23:51 +0900

    スタイル共通化コンポーネント専用のディレクトリを追加

---
+++ src/components/Parts/NuxtLink/Text.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLink v-bind="$attrs">
+    <slot />
+  </NuxtLink>
+</template>
+
+<style lang="scss" scoped>
+  a {
+    color: black !important;
+  }
+</style>
---
+++ src/components/Parts/UL.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <slot />
+  </ul>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+  ul {
+    list-style-type: none;
+    padding: 0 !important;
+    margin: 30px 0;
+  }
+</style>
