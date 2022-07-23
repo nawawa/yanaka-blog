a05f74df5566c5c2295012fcab45b31c5e69e477
nawawa <oonawa12@gmail.com>
2022-07-23 12:57:03 +0900

    v-paginationのlength指定

--- src/components/Pages/articles/index.vue
+++ src/components/Pages/articles/index.vue
@@ -7,6 +7,12 @@
       v-for="article in articles" :key="article.id"
       :article="article"
     />
+    <v-pagination
+      circle
+      color="background"
+      length="6"
+    >
+    </v-pagination>
   </div>
 </template>
 
