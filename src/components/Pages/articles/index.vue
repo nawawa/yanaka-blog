8f3e5096a02cd2084d6a9bf035448446ff7bb74c
nawawa <oonawa12@gmail.com>
2022-07-23 12:10:51 +0900

    レイアウトをトップページと共通化

--- src/components/Pages/articles/index.vue
+++ src/components/Pages/articles/index.vue
@@ -1,16 +1,12 @@
 <template>
   <div>
-    <h1>新着記事一覧</h1>
-    <UIResponsive class="px-4">
-      <v-row>
-        <v-col 
-          :cols="xs" 
-          v-for="article in articles" :key="article.index"
-        >
-          <ObjectsArticleListPage :article="article" />
-        </v-col>
-      </v-row>
-    </UIResponsive>
+    <PartsHeading>
+      新着記事一覧
+    </PartsHeading>
+    <ObjectsArticleTopPageCard  
+      v-for="article in articles" :key="article.id"
+      :article="article"
+    />
   </div>
 </template>
 
@@ -19,15 +15,5 @@ export default {
   props: {
     articles: Array
   },
-  computed: {
-    xs() {
-      const breakpoint = this.$vuetify.breakpoint.name;
-      return (breakpoint === `xs`) ? 
-        12: 
-        (breakpoint === `sm`) ?
-          6:
-          4;
-    },
-  }
 }
 </script>
